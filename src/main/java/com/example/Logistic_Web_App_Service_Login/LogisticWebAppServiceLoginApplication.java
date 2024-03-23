2408eb86c8aa8334bbdddf9b802796152f36495a
Nguyen Van <Vanhoa30903@gmail.com>
2024-03-23 22:05:00 +0700

    initial Spring Boot Config

---
+++ src/main/java/com/example/Logistic_Web_App_Service_Login/LogisticWebAppServiceLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.Logistic_Web_App_Service_Login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticWebAppServiceLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticWebAppServiceLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Logistic_Web_App_Service_Login/LogisticWebAppServiceLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Logistic_Web_App_Service_Login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticWebAppServiceLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
